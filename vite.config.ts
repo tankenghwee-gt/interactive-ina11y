37f0c1880062397a1d203b006d792744e354a775
Tan Keng Hwee <tan_keng_hwee@tech.gov.sg>
2025-12-18 11:10:31 +0800

    Remove extension build entrypoint from vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,17 +5,4 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {
-    rollupOptions: {
-      input: {
-        content: "src/content.tsx", // Your new entry point
-      },
-      output: {
-        entryFileNames: "[name].js", // Force name to be content.js
-        assetFileNames: "assets/[name].[ext]",
-      },
-    },
-    // Ensure CSS is injected into JS so we can use ?inline
-    cssCodeSplit: false,
-  },
 });
